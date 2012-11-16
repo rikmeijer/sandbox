37896e11383c84c1102d736ee36ccfda20ecfc57
Rik Meijer <r.meijer@jcgroep.nl>
2012-11-16 11:03:35 +0100

    Sorting sandbox test

--- sorting.php
+++ sorting.php
@@ -18,18 +18,26 @@ $array = array(
 	'durian' => array(
 		'human-name' => 'Durian',
 		'order' => 0
-	)
+	),
+	'eggplant' => array(
+		'human-name' => 'Aubergine',
+		'order' => 0
+	),
+	'fig' => array(
+		'human-name' => 'Vijg',
+		'order' => 1
+	),
 );
 
 print_r($array);
 
 uasort($array, function($fruit_a, $fruit_b) {
 	$order_comparison = strcasecmp($fruit_a['order'], $fruit_b['order']);
-	if ($order_comparison !== 0) {
-		return $order_comparison;
+	if ($order_comparison === 0) {
+		return strncasecmp($fruit_a['human-name'], $fruit_b['human-name'], 10);
 	}
 	
-	return strcasecmp($fruit_a['human-name'], $fruit_b['human-name']);
+	return $order_comparison;
 });
 
 print_r($array);

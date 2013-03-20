3c2ca599aaf47afecd39173386002d7eba14d249
Rik Meijer <rikmeijer@gmail.com>
2013-03-20 11:36:45 +0100

    soap onder javascript experiment

---
+++ soap/server.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?><bookstore><book><title>Everyday Italian</title><author>Giada De Laurentiis</author><year>2005</year></book></bookstore>

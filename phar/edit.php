556ba4feb78cc7b976f1f963e0cee966094a2c6f
Rik Meijer <rmeijer@saa.nl>
2013-06-28 14:24:09 +0200

    add phar test

--- phar/edit.php
+++ phar/edit.php
@@ -1,5 +1,12 @@
 <?php
-
-$phar = new Phar('/home/rik/Projects/phpunit.phar');
-
-var_dump($phar);
+try {
+    $phar = new Phar(dirname(__DIR__) . '/test.phar');
+    $phar['index.php'] = '<?php echo "Hello World"; ?>';
+    $phar->setStub('#!/usr/bin/php' . PHP_EOL . '<?php
+Phar::webPhar();
+__HALT_COMPILER(); ?>');
+    //$phar->convertToExecutable(Phar::PHAR); // creates myphar.phar
+} catch (Exception $e) {
+    // handle error here
+    echo $e->getMessage();
+}

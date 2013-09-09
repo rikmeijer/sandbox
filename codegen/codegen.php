5882ca1f74cf85e7cf8a39f93a05b9229028a0d5
Rik Meijer <rmeijer@saa.nl>
2013-09-09 11:05:25 +0200

    - code genereren experiment Signed-off-by:Rik Meijer <rmeijer@saa.nl>

---
+++ codegen/codegen.php
@@ -0,0 +1,6 @@
+<?php
+
+
+
+$class = new ReflectionClass('Test');
+
---
+++ instanceof.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$var = null;
+var_Dump($var instanceof stdClass);
+
+if (!$var instanceof stdClass) {
+    echo' yes!';
+}

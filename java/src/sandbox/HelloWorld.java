a6ba14e77d9af9dde3b31ca35cc1cbeade590243
Rik Meijer <rikmeijer@gmail.com>
2015-08-13 12:03:41 +0200

    test set-up with simple Hello World class

---
+++ java/src/sandbox/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package sandbox;
+
+/**
+ * @author hameijer
+ *
+ */
+public final class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
+
+}

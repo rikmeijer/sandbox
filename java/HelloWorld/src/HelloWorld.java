650513d9d2f911ea83f10e42dd1da69e021b44e8
Rik Meijer <rikmeijer@gmail.com>
2015-08-18 11:05:44 +0200

    HaHaHachoo...

--- java/HelloWorld/src/HelloWorld.java
+++ java/HelloWorld/src/HelloWorld.java
@@ -2,7 +2,11 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		for (int i = 0; i < 3; i++) {
+			System.out.print("Ha");
+		}
+		System.out.print("choo ");
+		System.out.print("Hello Rik");
 	}
 
 }

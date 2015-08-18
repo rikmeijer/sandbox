54c15cc603a2c37b34da370660313899415492f9
Rik Meijer <rikmeijer@gmail.com>
2015-08-18 11:09:02 +0200

    Variable sneezing enabled

--- java/HelloWorld/src/HelloWorld.java
+++ java/HelloWorld/src/HelloWorld.java
@@ -2,10 +2,13 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 3; i++) {
-			System.out.print("Ha");
+		int sneeze = 8;
+		if (sneeze > 0) {
+			for (int i = 0; i < sneeze; i++) {
+				System.out.print("Ha");
+			}
+			System.out.print("choo ");
 		}
-		System.out.print("choo ");
 		System.out.print("Hello Rik");
 	}
 
